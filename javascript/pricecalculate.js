6ae989eb957f0c2223aa2aeac7a6b7f526d2eb0b
Tanmoy Bir <tanmoy7468@gmail.com>
2023-08-20 17:36:39 +0600

    full html & css Done

---
+++ javascript/pricecalculate.js
@@ -0,0 +1,49 @@
+function clickCard1(){
+    const entry=document.getElementById('calculate');
+    const cardName1=document.getElementById('card-1');
+    const cardOne=cardName1.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardOne;
+    entry.appendChild(li);
+}
+function clickCard2(){
+    const entry=document.getElementById('calculate');
+    const cardName2=document.getElementById('card-2');
+    const cardTwo=cardName2.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardTwo;
+    entry.appendChild(li);
+}
+function clickCard3(){
+    const entry=document.getElementById('calculate');
+    const cardName3=document.getElementById('card-3');
+    const cardThree=cardName3.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardThree;
+    entry.appendChild(li);
+}
+function clickCard4(){
+    const entry=document.getElementById('calculate');
+    const cardName4=document.getElementById('card-4');
+    const cardFour=cardName4.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardFour;
+    entry.appendChild(li);
+}
+function clickCard5(){
+    const entry=document.getElementById('calculate');
+    const cardName5=document.getElementById('card-5');
+    const cardFive=cardName5.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardFive;
+    entry.appendChild(li);
+}
+function clickCard6(){
+    const entry=document.getElementById('calculate');
+    const cardName6=document.getElementById('card-6');
+    const cardSix=cardName6.innerText;
+    const li=document.createElement('li');
+    li.innerText=cardSix;
+    entry.appendChild(li);
+}
+
