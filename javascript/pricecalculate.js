6a2c63fd37447c9ecdee43b1c65e2a1105109c8e
Tanmoy Bir <tanmoy7468@gmail.com>
2023-08-21 13:54:41 +0600

    project almost done

--- javascript/pricecalculate.js
+++ javascript/pricecalculate.js
@@ -1,51 +1,4 @@
-// function clickCard1(){
-//     const entry=document.getElementById('calculate');
-//     const cardName1=document.getElementById('card-1');
-//     const cardOne=cardName1.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardOne;
-//     entry.appendChild(li);
-// }
-// function clickCard2(){
-//     const entry=document.getElementById('calculate');
-//     const cardName2=document.getElementById('card-2');
-//     const cardTwo=cardName2.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardTwo;
-//     entry.appendChild(li);
-// }
-// function clickCard3(){
-//     const entry=document.getElementById('calculate');
-//     const cardName3=document.getElementById('card-3');
-//     const cardThree=cardName3.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardThree;
-//     entry.appendChild(li);
-// }
-// function clickCard4(){
-//     const entry=document.getElementById('calculate');
-//     const cardName4=document.getElementById('card-4');
-//     const cardFour=cardName4.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardFour;
-//     entry.appendChild(li);
-// }
-// function clickCard5(){
-//     const entry=document.getElementById('calculate');
-//     const cardName5=document.getElementById('card-5');
-//     const cardFive=cardName5.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardFive;
-//     entry.appendChild(li);
-// }
-// function clickCard6(){
-//     const entry=document.getElementById('calculate');
-//     const cardName6=document.getElementById('card-6');
-//     const cardSix=cardName6.innerText;
-//     const li=document.createElement('li');
-//     li.innerText=cardSix;
-//     entry.appendChild(li);
-// }
+
 function clickCard(cardId){
     const entry=document.getElementById('calculate');
     const cardName=document.getElementById(cardId);
@@ -68,44 +21,39 @@ function priceCalculate(priceId){
    if(totalAmount > 0){
     purchaseButton.removeAttribute('disabled');
    }
+   const totalValue=document.getElementById('total');
+   totalValue.innerText=totalAmount.toFixed(2); 
    return totalPrice;
 }
+
+
+document.getElementById('apply-btn').addEventListener('click',function(){
+   let finalTotalValue=document.getElementById('total-price');
+   const finalTotalValueString=finalTotalValue.innerText;
+   let totalAmount1=parseFloat(finalTotalValueString);
+    //   Discount
+   let discountAmount=(20/100)*totalAmount1;
+   const discount=document.getElementById('discount');
+   discount.innerText=discountAmount.toFixed(2);
+        const finalTotal=totalAmount1-discountAmount;  
+        const totalValue=document.getElementById('total');
+        totalValue.innerText=finalTotal.toFixed(2);
+})
 document.getElementById('promo-code').addEventListener('click',function(){
     const setPromoCode=document.getElementById('set-promo-code');
     setPromoCode.value ='SELL200';
    let finalTotalElement=document.getElementById('total-price');
    const finalTotalElementString=finalTotalElement.innerText;
    const finalTotalElementValue=parseFloat(finalTotalElementString);
-   console.log(finalTotalElementValue);
    const applyButton=document.getElementById('apply-btn');
    if(finalTotalElementValue >= 200 && setPromoCode.value =='SELL200'){
     applyButton.removeAttribute('disabled');
    }
-   
-})
-   
-    
-   
-
-document.getElementById('apply-btn').addEventListener('click',function(){
-   let finalTotalValue=document.getElementById('total-price');
-   const finalTotalValueString=finalTotalValue.innerText;
-   let totalAmount1=parseFloat(finalTotalValueString);
-   console.log(finalTotalValue);
-    //   Discount
-   let discountAmount=(20/100)*totalAmount1;
-   const discount=document.getElementById('discount');
-   discount.innerText=discountAmount.toFixed(2);
-//    Total
-    const finalTotal=totalAmount1-discountAmount;
-//    
-   const totalValue=document.getElementById('total');
-   totalValue.innerText=finalTotal.toFixed(2); 
 })
   
-// document.getElementById('goHome-btn').addEventListener('click',function(){
-    
-// })
+document.getElementById('goHome-btn').addEventListener('click',function(){
+    window.location.href='index.html';
+})
 
 function clickCard1(){
         clickCard('card-1');
